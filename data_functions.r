72a6430fe8241aebdd067933e8a1ad62bfbb277c
erik 'quantitative warlock' shilts <erik.shilts@opower.com>
2011-06-30 13:17:31 -0400

    classes: column of classes of each variable in a dataframe; DFInfo: commonly requested information about a dataframe

--- data_functions.r
+++ data_functions.r
@@ -1,9 +1,27 @@
 # Handy functions for working with data
 
+############################
+# Functions to describe data
+
 # Sorted names of a dateset
 sn <- function(dat) {
   sort(names(dat))
 }
+
+# Class of each variable in a dataset
+classes <- function(dat) {
+  cbind(sapply(dat, class))
+}
+
+DFInfo <- function(dat) {
+  df <- data.frame(vars = names(dat),
+                   class = classes(dat),
+                   missing = sapply(dat, function(x) sum(is.na(x))))
+  df[order(df$vars),]
+}
+
+############################
+# Functions to change data
 
 # Remove duplicates from dataset
 RemoveDupes <- function(dat) {
